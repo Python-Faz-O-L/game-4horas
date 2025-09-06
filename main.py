67a857dfb549972008610c0ae025bfa066fc07a9
Python-Faz-O-L <akarinapereiragomes@gmail.com>
2025-09-06 11:54:28 -0300

    Create main.py
    Parte básica

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+
+tamanhoTela = (800,400)
+
+screen = pygame.display.set_mode(tamanhoTela)
+
+running = True
+while running:
+  # analisar eventos
+  if event.type = pygame.QUIT:
+  
+  # desenhar elementos
+  # atualizar tudo
+  pygame.display.update()
+
+pygame.quit()
